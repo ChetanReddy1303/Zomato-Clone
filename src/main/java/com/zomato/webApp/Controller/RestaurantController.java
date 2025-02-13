3ae648e3636eeb462a241c5afc011cb6437eb7a1
Chetan Reddy <322103310203@gvpce.ac.in>
2025-02-13 20:54:03 +0530

    applied crossorigin in backend

--- src/main/java/com/zomato/webApp/Controller/RestaurantController.java
+++ src/main/java/com/zomato/webApp/Controller/RestaurantController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import com.zomato.webApp.services.RestaurantService;
 
 @RestController
 @RequestMapping("/restaurants")     
+@CrossOrigin(origins = "https://zomatoo-clone.web.app")
 public class RestaurantController {
 
     @Autowired
